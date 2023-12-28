921c03d2fefc59e1137dee25c7a6ed1d0655646d
Hai Long <hailong97bn@gmail.com>
2023-12-28 15:17:47 +0700

    Add connect SSH

--- connectSSH.py
+++ connectSSH.py
@@ -1,85 +1,196 @@
-import telnetlib
-class TelnetHostUtil(telnetlib.Telnet):
-    def __init__(self, logService=None):
-        telnetlib.Telnet.__init__(self)
-        # Disable telnetlib debug printing.
-        self.set_debuglevel(0)
-        self._logService = logService
-        self._hostName = None
-        hostPort = None
-        self._hostPasswd = None
-        self._hostLoginName = None
 
-    def update_host_info(self, hostName, hostPort, hostPasswd=None, hostLoginName=None):
-        self._hostName = hostName
-        hostPort = hostPort
-        if hostPasswd and hostLoginName:
-            self._hostPasswd = hostPasswd
-            self._hostLoginName = hostLoginName
+import logging
+import queue
+import re
+import threading
+import time
+from abc import abstractmethod
+from typing import Any, Callable, Optional, Union, List, Pattern
 
-    # @brief: open a telnet connection to a host processor.
-    def open_host_connection(self, hostName=None, hostPort=None, timeout=None, hide=None):
-        if hostName and hostPort:
-            self._hostName = hostName
-            hostPort = hostPort
-        self.open(self._hostName, hostPort, timeout)
-        if not self.sock:
-            raise ValueError('Host connection open failed!')
-        else:
-            self.log('Host connection open succeeded!', hide)
+class OTCIError(Exception):
+    """Base class for OTCI Errors."""
+    pass
 
-    # @brief: log in the Linux system with specified name + passwd.
-    def host_login(self, hostLoginName, hostPasswd):
+class CommandError(OTCIError):
+    """OTCI failed to execute a command."""
+
+    def __init__(self, cmd: str, output: List[str]):
+        self.__output = output
+        super(CommandError, self).__init__("Command error while executing %r:\n%s\n" % (cmd, '\n'.join(output)))
+
+    def error(self) -> str:
+        return self.__output[-1]
+
+class OtbrSshCommandRunner():
+
+    def __init__(self, host, port, username, password, sudo=None, prefix = True):
+        import paramiko
+
+        self.__host = host
+        self.__port = port
+        self.__sudo = sudo
+        self.__username = username
+        self.__password = password
+        self.__prefix = prefix
+        self.__ssh = paramiko.SSHClient()
+        self.__ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        self.__line_read_callback = None
+        self.__maxByte = 327675
+
+        print("\r\nConnect to host {} port {}".format(host, port))
         try:
-            self._hostLoginName = hostLoginName
-            self._hostPasswd = hostPasswd
-            '''
-            # First, enter the login name when "raspberrypi login:"
-            # Second, enter the password when "Password:"
-            # Last, verify login using "hostLoginName@raspberrypi"
-            '''
-            self.read_until("raspberrypi login:", 5)
-            self.write(hostLoginName + "\n")
-            self.read_until("Password:", 5)
-            self.write(hostPasswd + "\n")
-            self.read_until(self._hostLoginName + "@", 5)
-            self.log('Host [' + self._hostLoginName + '] login succeeded!')
-            return 'success'
-        except EOFError:
-            self.log('Socket disconnected.')
-            return 'conn_failure'
-
-    # @brief: send commands to the host application with expected results.
-    #         Returns 'success' if match is found, otherwise return original
-    #         printouts.
-    def host_send_expect(self, cmd, expectStr, timeout=None, hide=None):
-        try:
-            self.log("Sending cmd [" + cmd + "]...", hide)
-            self.write(cmd + "\r\n")
-            buf = self.read_until(expectStr, timeout)
-            if expectStr in buf:
-                return 'success'
+            print("username:", username)
+            print("password:", password)
+            self.__ssh.connect(host,
+                               port=port,
+                               username=username,
+                               password=password,
+                               allow_agent=False,
+                               look_for_keys=False)
+        except paramiko.ssh_exception.AuthenticationException:
+            if not password:
+                self.__ssh.get_transport().auth_none(username)
             else:
-                msg = "Expected string [" + \
-                      expectStr + \
-                      "] not found. Original prints:"
-                # self.log(buf)
-                return msg + "\n" + buf
-        except EOFError:
-            self.log('Socket disconnected.')
-            return 'conn_failure'
+                raise
+        self.channel = self.__ssh.invoke_shell()
+        self.channel.settimeout(10)
 
-    # @brief: send commands to the host application and return results. No
-    #         check on the expected pattern.
-    def host_send(self, cmd, timeout=None, hide=None):
-        #try:
-        self.log("Sending cmd [" + cmd + "]...", hide)
-        self.write(cmd + "\r\n")
-        print("BUG Before:")
-        buf = self.read_until('Nonsense', timeout)
-        print("BUG After:", buf)
-        return buf
-        #except EOFError, IOError:
-        #    self.log('Socket disconnected.')
-        #    return ''
+    def __repr__(self):
+        return f'{self.__host}:{self.__port}'
 
+    def execute_command(self, cmd: str, timeout: float) -> List[str]:
+
+        if not self.__prefix:
+            sh_cmd = f'{cmd}'
+        else:
+            sh_cmd = f'ot-ctl -- {cmd}'
+        if self.__sudo:
+            sh_cmd = 'sudo ' + sh_cmd
+        print("\r\nsh_cmd: {}".format(sh_cmd))
+        cmd_in, cmd_out, cmd_err = self.__ssh.exec_command(sh_cmd, timeout=int(timeout), bufsize=1024)
+        err = cmd_err.read().decode('utf-8')
+        if err:
+            raise CommandError(cmd, [err])
+
+        output = [l.rstrip('\r\n') for l in cmd_out.readlines()]
+
+        if self.__line_read_callback is not None:
+            for line in output:
+                self.__line_read_callback(line)
+
+        if cmd in ('reset', 'factoryreset'):
+            self.wait(3)
+
+        return output
+
+    def send_cmd(self, cmd: str, timeout: float):
+        try:
+            recv_data = ''
+            cmd_out = ''
+            print("\r\ncmd: {}".format(cmd))
+            self.channel.send(cmd + "\n")
+            time.sleep(timeout-1)
+            recv_data =  self.channel.recv(self.__maxByte).decode('utf-8')
+            cmd_out += str(recv_data)
+            print("cmd_out", cmd_out)
+        except:
+            print('Socket disconnected.')
+            return ''
+
+    def send_cmd_expect(self, cmd: str, expectStr: str, timeout: float, breakTime: float):
+        print("\r\n[{}]cmd: {}".format(round(time.time(),2),cmd))
+        recv_data = ''
+        cmd_out = ''
+        result = False
+        self.channel.send(cmd + "\n")
+        startTime = time.time()
+        elapsedTime = 0
+        while(timeout > time.time() - startTime):
+            time.sleep(1)
+            if self.channel.recv_ready():
+                recv_data = self.channel.recv(self.__maxByte).decode('utf-8')
+                cmd_out += str(recv_data)
+                if expectStr in recv_data:
+                    # wait cmd finish
+                    if breakTime:
+                        time.sleep(breakTime)
+                        # when there's no data coming in,
+                        # recv will wait until the timeout set by setttimeout which is 10s now.
+                        # So use recv_ready() as the condition to rx data to avoid waiting 10s more.
+                        if self.channel.recv_ready():
+                            try:
+                                recv_data = self.channel.recv(self.__maxByte).decode('utf-8')
+                                cmd_out += str(recv_data)
+                            except:
+                                pass
+                    result = True
+                    break
+        elapsedTime = time.time() - startTime
+        # print("[{}] Cmd finish".format(round(time.time(),2)))
+        return result, str(cmd_out), elapsedTime
+
+    def recv(self, timeout: float):
+        data_out = ''
+        startTime = time.time()
+        elapsedTime = 0
+        while(timeout > elapsedTime):
+            time.sleep(0.5)
+            if self.channel.recv_ready():
+                recv_data = self.channel.recv(self.__maxByte).decode('utf-8')
+                data_out += str(recv_data)
+            elapsedTime = time.time() - startTime
+        print("elapsedTime: ", elapsedTime)
+        return str(data_out)
+
+    def recv_expect(self, expectStr: str, timeout: float):
+        result = False
+        startTime = time.time()
+        elapsedTime = 0
+        recv_data = ''
+        while(timeout > elapsedTime):
+            time.sleep(0.5)
+            if self.channel.recv_ready():
+                data = self.channel.recv(self.__maxByte).decode('utf-8')
+                recv_data += str(data)
+                if expectStr in recv_data:
+                    result = True
+                    break
+            elapsedTime = time.time() - startTime
+        return result, str(recv_data), elapsedTime
+
+    def close(self):
+        self.__ssh.close()
+
+    def wait(self, duration: float) -> List[str]:
+        time.sleep(duration)
+        return []
+
+    def set_line_read_callback(self, callback: Optional[Callable[[str], Any]]):
+        self.__line_read_callback = callback
+
+    def reopen_connect(self):
+        import paramiko
+        self.__ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            self.__ssh.connect(self.__host,
+                               port=self.__port,
+                               username=self.__username,
+                               password=self.__password,
+                               allow_agent=False,
+                               look_for_keys=False)
+        except paramiko.ssh_exception.AuthenticationException:
+            if not self.__password:
+                self.__ssh.get_transport().auth_none(self.__username)
+            else:
+                raise
+        self.channel = self.__ssh.invoke_shell()
+        self.channel.settimeout(10)
+
+if __name__ == "__main__":
+    host = "192.168.1.178"
+    port = 22
+    username = "ubuntu"
+    password = "solutions"
+    cmd_handler = OtbrSshCommandRunner(host, port, username, password)
+    cmd_handler.send_cmd(cmd="sudo ls -la", timeout=2)
--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-import  connectSSH
-
-sshRaspi = connectSSH.TelnetHostUtil
-hostIp = "192.168.1.1."
-hostPort = 23
-appPath = "local/path/"
-hostName = "ubuntu"
-hostPasswd = "1"
+import telnet
+import connectSSH
 
 if __name__ == "__main__":
-    print('Delete device.tbl and config.def file ')
-    sshRaspi.open_host_connection(hostIp, hostPort)
-    sshRaspi.host_login(hostName, hostPasswd)
-    sshRaspi.host_send('cd {}'.format(appPath), 2)
-    sshRaspi.host_send('sudo rm device.tbl', 1)
-    sshRaspi.host_send('sudo rm config.def', 1)
-    result = sshRaspi.host_send('ls', 1)
+    pass
