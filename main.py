bb8770e1db579d70d2f9da8b9c5c06636d61cc03
Nguyen Hai Long <67466062+hailong17@users.noreply.github.com>
2024-01-02 17:46:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import random 
+
 def sum(a,b):
 	return a+b
 
 if __name__ == '__main__':
-	print(sum(3,4))
+	a = random.random()
+	b = random.random()
+	print("number a is:", a)
+	print("number b is:", b)
+	print(sum(a,b))
